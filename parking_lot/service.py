d2128bd9f653fea1e6048f370a9a34b7ecc2cf66
wuttinanhi <wuttinanhi@gmail.com>
2022-09-22 17:27:50 +0000

    add get all parking lot with status method

--- parking_lot/service.py
+++ parking_lot/service.py
@@ -38,6 +38,68 @@ class ParkingLotService:
         return ParkingLot.query.filter(ParkingLot.open_status == True).all()
 
     @staticmethod
-    def is_available(parking_lot: ParkingLot):
-        # TODO: need implement
-        pass
+    def is_parking_lot_available(parking_lot: ParkingLot):
+        result = db_session.execute(
+            """
+                SELECT 
+                    *
+                FROM 
+                    (
+                        SELECT
+                            reservation.*,
+                            (
+                                CASE
+                                    WHEN reservation.end_time IS NULL THEN "parking"
+                                    ELSE "ended"
+                                END
+                            ) AS parking_status
+                        FROM 
+                            reservation
+                    ) AS t1 
+                WHERE
+                    t1.parking_status = "parking"
+                    AND
+                    t1.parking_lot_id = :parking_lot_id
+            """,
+            {"parking_lot_id": parking_lot.id}
+        )
+
+        return len(result.all()) == 0
+
+    @staticmethod
+    def get_all_parking_lot_with_available_status():
+        result = db_session.execute(
+            """
+            SELECT
+                DISTINCT t1.id,
+                t1.location,
+                t1.open_status,
+                ANY_VALUE(t1.available) AS available
+            FROM (
+                SELECT 
+                    parking_lot.id,
+                    parking_lot.location,
+                    parking_lot.open_status,
+                    (
+                        CASE
+                            WHEN reservation.end_time IS NOT NULL AND parking_lot.open_status = 1 THEN 1
+                            ELSE 0
+                        END
+                    ) AS available
+                FROM
+                    parking_lot
+                LEFT JOIN
+                    reservation
+                ON 
+                    parking_lot.id = reservation.parking_lot_id
+                ORDER BY
+                    parking_lot.id ASC
+            )  AS t1
+            GROUP BY
+                t1.id
+            ORDER BY 
+                t1.id ASC
+        """
+        )
+
+        return result.all()
