dc901553e41b0b773791378a99a4aba6961e2ef1
wuttinanhi <wuttinanhi@gmail.com>
2022-09-22 17:29:50 +0000

    force load env at database

--- database/database.py
+++ database/database.py
@@ -3,16 +3,28 @@
 """
 import os
 
+from env_wrapper import load_env
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.orm.query import Query
 from sqlalchemy.orm.session import Session
 
-engine = create_engine(os.getenv("DATABASE_URI"))
-db_session: Session = scoped_session(
-    sessionmaker(autocommit=False, autoflush=False, bind=engine)
-)
+load_env()
+
+
+def get_engine():
+    return create_engine(os.getenv("DATABASE_URI"))
+
+
+engine = get_engine()
+
+
+def get_db_session() -> Session:
+    return scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+
+db_session: Session = get_db_session()
 
 
 class Base():
